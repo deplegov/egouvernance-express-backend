15159eac71e494e30bee01550fd969030ca47bf0
jheri04 <139941100+jheri04@users.noreply.github.com>
2023-09-01 12:45:22 +0300

    add data

--- db.sql
+++ db.sql
@@ -91,6 +91,29 @@ CREATE TABLE commentaire (
 insert into article (id,titre,description,date_de_publication) values (article_sequence.NEXTVAL,'article1','ceci est un test 1',SYSDATE);
 insert into article (id,titre,description,date_de_publication) values (article_sequence.NEXTVAL,'article2','ceci est un test 2',SYSDATE);
 
+insert into article (id,titre,description,date_de_publication) values (
+    article_sequence.NEXTVAL,
+    'PALAIS D''AMBOHITSOROHITRA : RENCONTRE AVEC UNE DÉLÉGATION CORÉENNE',
+    'Après son déplacement dans la Région Vakinankaratra ce 28 août 2023, le Président de la République Andry Rajoelina a reçu au Palais d'État d'Ambohitsorohitra, une délégation de la République de Corée dirigée par M. YOON Sang Jick, envoyé spécial du Président de la République de Corée, Secrétaire Général du Comité de candidature pour l''exposition universelle 2030 à Busan. Une rencontre qui vise à renforcer la coopération bilatérale entre les deux pays et à explorer de nouvelles opportunités de partenariat économique.
+Madagascar a été choisi comme première destination par l''émissaire du Président coréen qui est accompagné pour ce déplacement, par des hauts responsables du top 10 des entreprises coréennes, qui sont tous des leaders mondiaux. Ce qui souligne l''importance de la place qu''occupe la Grande Île dans le développement des relations entre l''Afrique et la Corée du Sud.'
+    ,SYSDATE);
+
+insert into article (id,titre,description,date_de_publication) values (
+    article_sequence.NEXTVAL,
+    'Coup d''envoi de la 11ème édition des jeux des îles de l''Océan Indien.',
+    'La cérémonie d''ouverture des jeux des îles de l''Océan Indien s''est tenue ce 25 août 2023 au Kianja Barea Mahamasina. Une cérémonie grandiose sous la houlette du Président de la République Andry Rajoelina et de son épouse Mialy Rajoelina. Parmi les invités d''honneur de la cérémonie figurent le Président de l''Union des Comores, Azali Assoumani, le Président des Seychelles, Wavel Ramkalawan, ainsi que le Président de l''Île Maurice, Prithvirajsing Roopun. Un des points d''orgue de la cérémonie fut le défilé de près de 4213 athlètes de la Réunion, l''île Maurice, les Seychelles, les Comores, Mayotte, les Maldives et de Madagascar.
+
+S''en est suivi de la série d''allocution à laquelle ont pris part le Ministre de la Jeunesse et des Sports, André Resampa, le Président du Conseil Internationale des Jeux des Îles, Antonio Gopal et le Président de la République Andry Rajoelina. Après avoir salué l''ensemble des athlètes malagasy et réitéré ses bénédictions, le Président Andry Rajoelina a appelé à une minute de silence suite au décès de plusieurs personnes après une bousculade devant un portail du Stade. Le Premier Ministre ainsi que plusieurs membres du Gouvernement se sont d''ailleurs immédiatement rendus au chevet des victimes. Les personnes blessées seront prises en charge par l''Etat. Le Chef de l''Etat Andry Rajoelina a adressé un message de réconfort aux victimes et a saisi l''occasion pour interpeller les différents responsables afin qu''un tel drame ne puisse plus se reproduire.'
+    ,SYSDATE);
+
+insert into article (id,titre,description,date_de_publication) values (
+    article_sequence.NEXTVAL,
+    'Antsiranana : Accueil de la flamme des Jeux des Île de l''Océan Indien',
+    'Le Président de la République Andry Rajoelina a entamé la deuxième journée de son déplacement à Antsiranana par une visite au camp pénal d''Andaloasy à Antsiranana, ce samedi 12 août 2023. L''amélioration des conditions carcérales fait partie des défis lancés par le Président Andry Rajoelina. Des reformes ont d''ailleurs été apportées au sein de l''Administration pénitentiaire, pour assurer une meilleure réintégration des prisonniers au niveau de la société après leur incarcération. Ceci à travers diverses formations telles que les formations agricoles, la menuiserie ou la couture pour les femmes détenues.
+
+Concernant le camp pénal d''Andaloasy, le terrain a été exploité par l''Administration pénitentiaire depuis 1982. A court et moyen terme, l''objectif y est d''améliorer en qualité et en quantité l''alimentation des détenues. En revanche, pour le long terme, il s''agit de garantir l''autosuffisance alimentaire dans la prison. Les détenus sont sélectionnés en fonction de leurs bonnes conduites. Au début, ils reçoivent une formation sur l''agriculture et travaillent par la suite sur une surface de 108Ha où ils cultivent le riz, le maïs, le manioc et des légumineuses. Le Chef de l''Etat a profité de cette descente pour remettre des uniformes aux prisonniers sélectionnés.'
+    ,SYSDATE);
+
 -- insert into societe values (societe_sequence.NEXTVAL,'societe1','1234','1234','mdp');
 
 insert into utilisateur (id,nom,prenom,email,password,role,type) values 
